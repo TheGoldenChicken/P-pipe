9adb39d418abcf2836b4a5e6c1d0021d5fa6b986
TheGoldenChicken <karlmeisner99@gmail.com>
2025-09-19 16:23:16 +0200

    chore: small cleanup in dispatcher code

--- backend/src/endpoints/dispatcher.rs
+++ backend/src/endpoints/dispatcher.rs
@@ -1,13 +1,10 @@
-use rocket::execute;
 use rocket::serde::{ Deserialize, Serialize, json::Json };
 use rocket::{ State, response::status::Custom, http::Status };
 use tokio_postgres::Client;
-use std::ops::Range;
-// use postgres_types::{ ToSql, FromSql };
 
-// TODO: Consider if an ORM (diesel, sqlx, sea-orm) makes sense?
+// TODO: Consider if we should do away with the whole Custom<String> Type and instead just use tokio_postgres::Error?
+// TODO: Consider if we should look into a way of having custom return messages... what if we want to show both challenges affected, AND transactions?
 
-use std::process::Command;
 
 // TODO: To it possible to have a more a general "get n from database" function, consider implementing a FromRow trait for Challenge and all other relevant classes
 #[derive(Serialize, Deserialize, Clone)]
@@ -81,23 +78,18 @@ pub async fn add_challenge(
                                 release_proportions: row.get("release_proportions"),
                                 time_between_releases: row.get("time_between_releases"),
                                 })?;
-
+    
+    // Generate transactions and add them to the DB
     let generated_transactions = transactions_from_challenge(challenge);
-    // println!("{:?}", generated_transactions);
-    add_transactions_into_db(conn, generated_transactions).await.map_err(|e| Custom(Status::InternalServerError, e.to_string()))?;
+    add_transactions_into_db(conn, generated_transactions).await?;
 
-    // TODO: Consider if we should look into a way of having custom return messages... what if we want to show both challenges affected, AND transactions?
-    get_challenges(conn).await
-
-    // TODO: Add logic for handling if there is already an existing query? Add logic to create transactions automatically
+    get_challenges(conn).await                            
 }
-
-// TODO: Consider if we should do away with the whole Custom<String> Type and instead just use tokio_postgres::Error?
 
 pub async fn add_transactions_into_db(
     conn: &Client,
     transactions: Vec<Transaction>,
-) -> Result<u64, tokio_postgres::Error> {
+) -> Result<u64, Custom<String>> {
     if transactions.is_empty() {
         return Ok(0);
     }
@@ -127,26 +119,9 @@ pub async fn add_transactions_into_db(
         params.push(&tx.rows_to_push);
     }
 
-    // println!("Query attempted: {:?}", query);
-    // println!("Params used: {:?}", params);
-
-    // Execute the bulk insert
-    // execute_query(conn, &query, &params).await
-    conn.execute(&query, &params).await
+    execute_query(conn, &query, &params).await
 }
 
-pub async fn add_transation_into_db(
-    conn: &State<Client>,
-    transaction: Transaction
-) -> Result<u64, Custom<String>> {
-    execute_query(conn,
-    "INSERT INTO transactions
-    (challenge_id, scheduled_time, source_data_location, data_intended_location, rows_to_push)
-    VALUES ($1, $2, $3, $4, $5)",
-    &[&transaction.challenge_id, &transaction.scheduled_time, &transaction.source_data_location,
-            &transaction.data_intended_location, &transaction.rows_to_push]
-    ).await
-}
 
 fn transactions_from_challenge(challenge: Challenge) -> Vec<Transaction> {
     let mut transactions = Vec::new();
@@ -167,21 +142,6 @@ fn transactions_from_challenge(challenge: Challenge) -> Vec<Transaction> {
         let rows_to_push = vec![rows_from, rows_to];
         running_proportion += release_proportion;
         
-        // let rows_to_push_count = (release_proportion * challenge.init_dataset_rows as f64).round() as i32;
-
-        // // Define the chunk size for each range (e.g. 50 rows per range)
-        // let chunk_size = 50;
-
-        // // Generate ranges like [0..50), [50..100), ...
-        // let ranges = (0..rows_to_push_count)
-        //     .step_by(chunk_size as usize)
-        //     .map(|start| {
-        //         let end = (start + chunk_size).min(rows_to_push_count);
-        //         start..end
-        //     })
-        //     .collect::<Vec<Range<i32>>>();
-
-        // let rows_to_push = structs::Int4RangeArray(ranges);
 
         let transaction = Transaction {
             id: None,
@@ -202,42 +162,6 @@ fn transactions_from_challenge(challenge: Challenge) -> Vec<Transaction> {
     transactions
 }
 
-
-#[get("/api/transactions")]
-pub async fn get_transactions(conn: &State<Client>) -> Result<Json<Vec<Transaction>>, Custom<String>> {
-    get_transactions_from_db(conn).await.map(Json)
-}
-
-
-// TODO: Find out specifically why we don't need to call Some on status, attempted_at ,stdout, stderr, and so on...
-async fn get_transactions_from_db(client: &Client) -> Result<Vec<Transaction>, Custom<String>> {
-    let transactions = client
-        .query("SELECT * FROM transactions", &[])
-        .await
-        .map_err(|e| Custom(Status::InternalServerError, e.to_string()))?
-        .iter()
-        .map(|row| Transaction {
-            id: Some(row.get("id")),
-            challenge_id: row.get("challenge_id"),
-            created_at: row.get("created_at"),
-            scheduled_time: row.get("scheduled_time"),
-            source_data_location: row.get("source_data_location"),
-            data_intended_location: row.get("data_intended_location"),
-            rows_to_push: row.get("rows_to_push"),
-            attempted_at: row.get("attempted_at"),
-            status: row.get("status"),
-            stdout: row.get("stdout"),
-            stderr: row.get("stderr"),
-        })
-        .collect::<Vec<Transaction>>();
-    Ok(transactions)
-}
-
-#[delete("/api/transactions/<id>")]
-pub async fn delete_transaction(conn: &State<Client>, id: i32) -> Result<Status, Custom<String>> {
-    execute_query(conn, "DELETE FROM transactions WHERE id = $1", &[&id]).await?;
-    Ok(Status::NoContent)
-}
 
 
 #[get("/api/challenges")]
@@ -272,45 +196,58 @@ pub async fn delete_challenge(conn: &State<Client>, id: i32) -> Result<Status, C
     Ok(Status::NoContent)
 }
 
+#[get("/api/transactions")]
+pub async fn get_transactions(conn: &State<Client>) -> Result<Json<Vec<Transaction>>, Custom<String>> {
+    get_transactions_from_db(conn).await.map(Json)
+}
 
-// // TODO: Find actually logical names for these...
-// #[post("/api/assignments/temp")] 
-// pub async  fn call_dispatcher() {
+// TODO: ADD THESE TO THE CORS, AND TEST THEM!
 
-//     // let output = Command::new("pytest").output().expect("failed to run pytest!");
+// TODO: Find out specifically why we don't need to call Some on status, attempted_at ,stdout, stderr, and so on...
+async fn get_transactions_from_db(client: &Client) -> Result<Vec<Transaction>, Custom<String>> {
+    let transactions = client
+        .query("SELECT * FROM transactions", &[])
+        .await
+        .map_err(|e| Custom(Status::InternalServerError, e.to_string()))?
+        .iter()
+        .map(|row| Transaction {
+            id: Some(row.get("id")),
+            challenge_id: row.get("challenge_id"),
+            created_at: row.get("created_at"),
+            scheduled_time: row.get("scheduled_time"),
+            source_data_location: row.get("source_data_location"),
+            data_intended_location: row.get("data_intended_location"),
+            rows_to_push: row.get("rows_to_push"),
+            attempted_at: row.get("attempted_at"),
+            status: row.get("status"),
+            stdout: row.get("stdout"),
+            stderr: row.get("stderr"),
+        })
+        .collect::<Vec<Transaction>>();
+    Ok(transactions)
+}
 
-//     let output = Command::new("python") // or "python" depending on your system
-//         .args(&[
-//             "module/dispatcher.py",
-//             "--input_file", "tests/test_data/rust_splits/split_1.csv",
-//             "--output_dir", "tests/test_data/rust_dispatched",
-//             "--merge_files", "False",
-//         ])
-//         .output()
-//         .expect("Failed to execute Python script");
+pub async fn add_transation_into_db(
+    conn: &State<Client>,
+    transaction: Transaction
+) -> Result<u64, Custom<String>> {
+    execute_query(conn,
+    "INSERT INTO transactions
+    (challenge_id, scheduled_time, source_data_location, data_intended_location, rows_to_push)
+    VALUES ($1, $2, $3, $4, $5)",
+    &[&transaction.challenge_id, &transaction.scheduled_time, &transaction.source_data_location,
+            &transaction.data_intended_location, &transaction.rows_to_push]
+    ).await
+}
 
-//     println!("Status: {}", output.status);
-//     println!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
-//     println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
-// }
 
-// #[post("/api/assignments")] 
-// pub async fn call_file_splitter() {
+#[delete("/api/transactions/<id>")]
+pub async fn delete_transaction(conn: &State<Client>, id: i32) -> Result<Status, Custom<String>> {
+    execute_query(conn, "DELETE FROM transactions WHERE id = $1", &[&id]).await?;
+    Ok(Status::NoContent)
+}
 
-//     // let output = Command::new("pytest").output().expect("failed to run pytest!");
 
-//     let output = Command::new("python") // or "python" depending on your system
-//         .args(&["module/file_splitter.py",
-//                 "--csv_file", "tests/test_data/iris.csv",
-//                 "--proportions", "0.5,0.25,0.25",
-//                 "--output_dir", "tests/test_data/rust_splits"]) // path to your Python script
-//         .output()
-//         .expect("Failed to execute Python script");
-
-//     println!("Status: {}", output.status);
-//     println!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
-//     println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
-// }
 
 async fn execute_query(
     client: &Client,
