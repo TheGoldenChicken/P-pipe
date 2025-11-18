0a17d9ebc0c014abbc962957fd9afb7716724f2c
TheGoldenChicken <karlmeisner99@gmail.com>
2025-11-18 17:48:44 +0100

    chore: fix small error with adding double .json extension for instance_saver

--- backend/src/bin/instance_saver.rs
+++ backend/src/bin/instance_saver.rs
@@ -4,11 +4,11 @@ use backend::testing_common::save_instance::save_instance;
 
 // TODO: Minor changes to this so it correctly saves to py_modules/tests/json_instances
 fn main() -> std::io::Result<()> {
-    save_instance(&challenge_instance(), "challenge_instance.json")?;
-    save_instance(&minimal_challenge_instance(), "minimal_challenge_instance.json")?;
-    save_instance(&challenge_instance_multiple_dispatch(), "challenge_instance_multiple_dispatch.json")?;
-    save_instance(&transaction_instance(), "transaction_instance.json")?;
-    save_instance(&transactions_expected_from_challenge_instance(), "transactions_expected_from_challenge_instance.json")?;
+    save_instance(&challenge_instance(), "challenge_instance")?;
+    save_instance(&minimal_challenge_instance(), "minimal_challenge_instance")?;
+    save_instance(&challenge_instance_multiple_dispatch(), "challenge_instance_multiple_dispatch")?;
+    save_instance(&transaction_instance(), "transaction_instance")?;
+    save_instance(&transactions_expected_from_challenge_instance(), "transactions_expected_from_challenge_instance")?;
 
     Ok(())
 }
