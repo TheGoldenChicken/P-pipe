b542ee9b3fced3281b0f98581a16070928f3ec07
TheGoldenChicken <karlmeisner99@gmail.com>
2025-12-05 14:33:54 +0100

    add schema support for completed requests, db and backend

--- backend/src/migrations/004_create_requests_schema.sql
+++ backend/src/migrations/004_create_requests_schema.sql
@@ -1,6 +1,4 @@
--- Only used for requests, hence why they are not in 001_create_enums.sql
-CREATE TYPE request_type as ENUM ('data_validation', 'batch_prediction', 'calculated_feature');
-CREATE TYPE request_status as ENUM ('correct', 'partial_correct', 'incorrect', 'syntax_error', 'deadline_exceeded');
+CREATE TYPE request_status as ENUM ('pending', 'correct', 'partial_correct', 'incorrect', 'syntax_error', 'deadline_exceeded');
 
 CREATE TABLE IF NOT EXISTS requests (
     id SERIAL PRIMARY KEY,
@@ -14,20 +12,19 @@ CREATE TABLE IF NOT EXISTS requests (
     deadline BIGINT
 );
 
--- Focus on one table at a time!
+CREATE TABLE IF NOT EXISTS completed_requests (
+    id SERIAL PRIMARY KEY,
+    challenge_id INTEGER NOT NULL REFERENCES challenges(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    created_at BIGINT DEFAULT EXTRACT(EPOCH FROM now()),
 
--- CREATE TABLE IF NOT EXISTS completed_requests (
---     id SERIAL PRIMARY KEY,
---     challenge_id INTEGER NOT NULL REFERENCES challenges(id) ON DELETE CASCADE ON UPDATE CASCADE,
---     created_at BIGINT DEFAULT EXTRACT(EPOCH FROM now()),
-    
---     type_of_request TEXT NOT NULL,
---     request_payload jsonB NOT NULL,
---     expected_response jsonB NOT NULL,
---     deadline BIGINT,
+    type_of_request jsonB NOT NULL,
+    expected_response jsonB NOT NULL,
+    deadline BIGINT,
 
---     request_status request_status NOT NULL,
---     submitted_at BIGINT
---     submitted_response jsonB,
+    request_status request_status NOT NULL,
+    submitted_at BIGINT,
+    submitted_response jsonB
+);
 
--- );
+-- Only used for requests, hence why they are not in 001_create_enums.sql
+-- CREATE TYPE request_type as ENUM ('data_validation', 'batch_prediction', 'calculated_feature');
