e38dcb3a56476b1234e189b9f9152f217b218692
TheGoldenChicken <karlmeisner99@gmail.com>
2025-12-05 14:33:54 +0100

    add schema support for completed requests, db and backend

--- backend/src/schemas/request.rs
+++ backend/src/schemas/request.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-
+use std::fmt;
 use rocket::serde::{Deserialize, Serialize};
 use sqlx::types::Json;
 
@@ -16,13 +16,13 @@ use sqlx::types::Json;
 #[derive(sqlx::Type, Serialize, Deserialize, Debug, Clone, PartialEq)]
 #[sqlx(type_name = "request_status", rename_all = "snake_case")]
 pub enum RequestStatus {
+    Pending,
     Correct,
     PartialCorrect,
     Incorrect,
     SyntaxError,
     DeadlineExceeded
 }
-
 
 // Cannot map to a postgres type because it is a tuple kinda type! Be aware!
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
@@ -31,6 +31,16 @@ pub enum RequestType {
     DataValidation(DataValidationPayload),
     BatchPrediction(BatchPredictionPayload),
     CalculatedFeature(CalculatedFeaturePayload)
+}
+
+impl fmt::Display for RequestType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RequestType::DataValidation(_) => write!(f, "DataValidation"),
+            RequestType::BatchPrediction(_) => write!(f, "BatchPrediction"),
+            RequestType::CalculatedFeature(_) => write!(f, "CalculatedFeature"),
+        }
+    }
 }
 
 // TODO: Consider if we need to make it a kind of Vec<HashMap<i32, HashMap<String, serde_json::Value>>>
@@ -72,21 +82,42 @@ pub struct Request {
     pub deadline: Option<i64>,
 }
 
-// #[derive(Serialize, Deserialize, Clone, Debug, sqlx::FromRow)]
-// pub struct CompletedRequest {
-//     pub id: Option<i32>,
-//     pub challenge_id: i32,
-//     pub created_at: Option<i64>,
+#[derive(Serialize, Deserialize, Clone, Debug, sqlx::FromRow)]
+pub struct CompletedRequest {
+    pub id: Option<i32>,
+    pub challenge_id: i32,
+    pub created_at: Option<i64>,
 
-//     // Transaction info fields
-//     pub type_of_request: RequestType,
-//     pub request_payload: ,
-//     pub expected_response: ,
-//     pub deadline: Option<i64>,
+    // Transaction info fields
+    pub type_of_request: Json<RequestType>,
+    pub expected_response: Json<RequestType>,
+    pub deadline: Option<i64>,
 
-//     pub request_status: RequestStatus,
-//     pub submitted_at: Option<i64>,
-//     pub submitted_response: ,
-// }
+    pub request_status: RequestStatus,
+    pub submitted_at: Option<i64>,
+    pub submitted_response: Option<Json<RequestType>>
+}
+
+impl CompletedRequest {
+    pub fn from_request(
+        req: Request,
+        status: RequestStatus,
+        submitted_at: Option<i64>,
+        submitted_response: Json<RequestType>,
+    ) -> Self {
+        CompletedRequest {
+            id: req.id,
+            challenge_id: req.challenge_id,
+            created_at: req.created_at,
+            type_of_request: req.type_of_request,
+            expected_response: req.expected_response,
+            deadline: req.deadline,
+            request_status: status,
+            submitted_at: submitted_at,
+            submitted_response: Some(submitted_response),
+        }
+    }
+}
+
 
 // TODO: Potentially implement from_request for CompletedRequest...
