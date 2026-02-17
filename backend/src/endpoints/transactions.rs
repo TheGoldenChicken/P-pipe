44b3aa1508ba8b537384848cb9670fe6ea175bc2
TheGoldenChicken <karlmeisner99@gmail.com>
2026-02-17 13:48:37 +0100

    feat: Add endpoint to get completed transactions

--- backend/src/endpoints/transactions.rs
+++ backend/src/endpoints/transactions.rs
@@ -4,8 +4,8 @@ use rocket::{http::Status, response::status::Custom};
 use rocket_db_pools::Connection;
 use sqlx::types::Json as DbJson;
 
-use crate::schemas::common::{AccessBinding, Db, DispatchTarget};
-use crate::schemas::transaction::Transaction;
+use crate::schemas::common::{AccessBinding, Db, DispatchTarget, TransactionStatus};
+use crate::schemas::transaction::{CompletedTransaction, Transaction};
 use crate::schemas::challenge::ChallengeOptions;
 
 
@@ -38,6 +38,42 @@ pub async fn get_transactions(
 
     Ok(Json(transactions))
 }
+
+#[get("/api/completed_transactions")]
+pub async fn get_completed_transactions(
+    mut db: Connection<Db>,
+) -> Result<Json<Vec<CompletedTransaction>>, Custom<String>> {
+    let transactions = sqlx::query_as!(
+        CompletedTransaction,
+        r#"
+        SELECT
+            id,
+            challenge_id,
+            created_at,
+            scheduled_time,
+            source_data_location,
+            dispatch_location as "dispatch_location: DispatchTarget",
+            data_intended_location,
+            data_intended_name,
+            rows_to_push,
+            access_bindings as "access_bindings: DbJson<Vec<AccessBinding>>",
+            challenge_options as "challenge_options: DbJson<ChallengeOptions>",
+            attempted_at,
+            transaction_status as "transaction_status: TransactionStatus",
+            stdout,
+            stderr
+        FROM 
+            completed_transactions
+        "#
+    )
+    .fetch_all(&mut **db)
+    .await
+    .map_err(|e| Custom(Status::InternalServerError, e.to_string()))?;
+
+    Ok(Json(transactions))
+}
+
+
 
 #[delete("/api/transactions/<id>")]
 pub async fn delete_transaction(mut db: Connection<Db>, id: i32) -> Result<Status, Custom<String>> {
